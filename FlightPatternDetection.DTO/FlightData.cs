0996f04fe46f01df2d01226dcf117994cdb4aa87
Jonas Kaad <36827320+JonasKaad@users.noreply.github.com>
2023-03-15 09:16:39 +0100

    Created FlightData DTO

---
+++ FlightPatternDetection.DTO/FlightData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightPatternDetection.DTO
+{
+    public class FlightData
+    {
+        public string AircraftICAO { get; set; } //Called aircraftType in the JSON
+        public string AircraftId { get; set; }
+        public string AircraftRegistration { get; set; }
+        public string AirportOrigin { get; set; }
+        public string AirportDestination { get; set; }
+
+
+    }
+}
