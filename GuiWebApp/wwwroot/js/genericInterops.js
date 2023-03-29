907951376bddbc5bca681fde0786352fb9da162b
Alexander Nørup <me@alexandernorup.com>
2023-03-29 12:24:55 +0200

    Added mass detection page and ability to load flights from a list. And to download flights viewed

---
+++ GuiWebApp/wwwroot/js/genericInterops.js
@@ -0,0 +1,19 @@
+
+async function downloadFileFromStream(fileName, contentStreamReference) {
+    const arrayBuffer = await contentStreamReference.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = fileName ?? '';
+    anchorElement.click();
+    anchorElement.remove();
+    URL.revokeObjectURL(url);
+}
+
+function simulateClick(id) {
+    let el = document.getElementById(id);
+    if (el != null) {
+        el.click();
+    }
+}
