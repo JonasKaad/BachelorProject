3954096288aa05ec66ec5e7d259ae937a16f1a45
Jonas Kaad <jonaskaad@gmail.com>
2023-03-29 15:14:45 +0200

    Removed unused endpoint

--- FlightPatternDetection/Controllers/NavDbController.cs
+++ FlightPatternDetection/Controllers/NavDbController.cs
@@ -1,8 +1,6 @@
 ﻿using FlightPatternDetection.DTO.NavDBEntities;
 using FlightPatternDetection.Services;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using System.Runtime.Intrinsics.Arm;
 
 namespace FlightPatternDetection.Controllers
 {
@@ -20,38 +18,11 @@ namespace FlightPatternDetection.Controllers
         }
 
         [HttpGet("Waypoints")]
-        public IEnumerable<EWayPoint> GetWayPoints(double lat, double lng, double radius=0.3)
+        public IEnumerable<EWayPoint> GetWayPoints(double lat, double lng, double radius = 0.3)
         {
-            var s = Stopwatch.StartNew();
             // How big of radius around the last point, that the waypoints should be fetched for
             var wayPoints = _navDbManager.Waypoints.FindAll(x => ((x.Latitude + radius) >= lat && lat >= (x.Latitude - radius)) && (x.Longitude + radius >= lng && lng >= (x.Longitude - radius))).ToList();
-            s.Stop();
-            _logger.LogDebug($"Found {wayPoints.Count} points in {s.Elapsed}");
-
             return wayPoints;
-
-        }
-
-        [HttpGet("getWayPointCoordinates")]
-        public List<List<Double>> GetWayPointCoordinates(double lat, double lng)
-        {
-            var s = Stopwatch.StartNew();
-
-            var wayPoints = _navDbManager.Waypoints.FindAll(x => ((x.Latitude + 1) >= lat && lat >= (x.Latitude - 1)) && (x.Longitude + 1 >= lng && lng >= (x.Longitude))).ToList();
-            s.Stop();
-            _logger.LogDebug($"Found {wayPoints.Count} points in {s.Elapsed}");
-
-            var coordinates = new List<List<Double>>();
-
-            foreach (var wayPoint in wayPoints)
-            {
-                var temp = new List<Double>();
-                temp.Add(wayPoint.Latitude);
-                temp.Add(wayPoint.Longitude);
-                coordinates.Add(temp);
-            }
-
-            return coordinates;
 
         }
 
