22c0e1347cb3e05593d4c9d653574eb44ec3d3cc
Jonas Kaad <jonaskaad@gmail.com>
2023-03-29 10:47:31 +0200

    Added option for drawing waypoints in interops

--- GuiWebApp/wwwroot/js/leafletInterops.js
+++ GuiWebApp/wwwroot/js/leafletInterops.js
@@ -1,33 +1,43 @@
 var map;
 
 var osmLayer = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution: "© OpenStreetMap",
+    maxZoom: 19,
+    attribution: "© OpenStreetMap",
 });
 
 var baseMap = {
-  OpenStreetMap: osmLayer,
+    OpenStreetMap: osmLayer,
 };
 
 var layerControl;
 
 async function createMap(lat, lng, zoomValue) {
-  if (map !== undefined) {
-    map.remove();
-  }
-  map = L.map("map", {
-    center: [lat, lng],
-    zoom: zoomValue,
-    layers: [osmLayer],
-  });
+    if (map !== undefined) {
+        map.remove();
+    }
+    map = L.map("map", {
+        center: [lat, lng],
+        zoom: zoomValue,
+        layers: [osmLayer],
+    });
 }
 
 async function addPoint(lat, lng) {
-  L.marker([lat, lng]).addTo(map);
+    L.marker([lat, lng]).addTo(map);
+}
+
+async function addWayPoints(lat, lng) {
+    var myIcon = L.icon({
+        iconUrl: 'waypoint.svg',
+        iconSize: [10, 20],
+        iconAnchor: [22, 94],
+        popupAnchor: [-3, -76],
+    });
+    L.marker([lat, lng], { icon: myIcon }).addTo(map);
 }
 
 async function createPath(coordinates, color) {
-  var polyline = L.polyline(coordinates, { color: color }).addTo(map);
-  L.featureGroup(getArrows(coordinates, 'black', 1, map)).addTo(map);
-  map.fitBounds(polyline.getBounds());
+    var polyline = L.polyline(coordinates, { color: color }).addTo(map);
+    L.featureGroup(getArrows(coordinates, 'black', 1, map)).addTo(map);
+    map.fitBounds(polyline.getBounds());
 }
