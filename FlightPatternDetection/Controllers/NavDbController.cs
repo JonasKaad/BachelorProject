6767df881b2de1fd5d5d5adc71b8712d7b55b208
Jonas Kaad <jonaskaad@gmail.com>
2023-03-29 10:24:23 +0200

    Added function to return only lat lngs

--- FlightPatternDetection/Controllers/NavDbController.cs
+++ FlightPatternDetection/Controllers/NavDbController.cs
@@ -19,22 +19,41 @@ namespace FlightPatternDetection.Controllers
             _navDbManager = navDbManager;
         }
 
-        [HttpGet("getLat")]
-        public IEnumerable<EWayPoint> getLatPoints(double lat, double lng)
+        [HttpGet("getWaypoints")]
+        public IEnumerable<EWayPoint> GetWayPoints(double lat, double lng)
         {
             var s = Stopwatch.StartNew();
 
-
-            var latPoints = _navDbManager.Waypoints.FindAll(x => ((x.Latitude + 1) >= lat && lat >= (x.Latitude - 1)) && (x.Longitude + 1 >= lng && lng >= (x.Longitude))).ToList();
+            var wayPoints = _navDbManager.Waypoints.FindAll(x => ((x.Latitude + 1) >= lat && lat >= (x.Latitude - 1)) && (x.Longitude + 1 >= lng && lng >= (x.Longitude))).ToList();
             s.Stop();
-            _logger.LogDebug($"Found {latPoints.Count} points in {s.Elapsed}");
+            _logger.LogDebug($"Found {wayPoints.Count} points in {s.Elapsed}");
 
-            return latPoints;
-
-            //return _navDbManager.Waypoints.Take(count);
+            return wayPoints;
 
         }
 
+        [HttpGet("getWayPointCoordinates")]
+        public List<List<Double>> GetWayPointCoordinates(double lat, double lng)
+        {
+            var s = Stopwatch.StartNew();
+
+            var wayPoints = _navDbManager.Waypoints.FindAll(x => ((x.Latitude + 1) >= lat && lat >= (x.Latitude - 1)) && (x.Longitude + 1 >= lng && lng >= (x.Longitude))).ToList();
+            s.Stop();
+            _logger.LogDebug($"Found {wayPoints.Count} points in {s.Elapsed}");
+
+            var coordinates = new List<List<Double>>();
+
+            foreach (var wayPoint in wayPoints)
+            {
+                var temp = new List<Double>();
+                temp.Add(wayPoint.Latitude);
+                temp.Add(wayPoint.Longitude);
+                coordinates.Add(temp);
+            }
+
+            return coordinates;
+
+        }
 
     }
 }
