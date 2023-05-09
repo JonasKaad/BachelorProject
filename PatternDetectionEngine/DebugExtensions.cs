6937d2f9029da1881e5ca968a42f74f6690bcd5e
Alexander Nørup <me@alexandernorup.com>
2023-05-09 12:46:39 +0200

    Improved detection engine

---
+++ PatternDetectionEngine/DebugExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using TrafficApiClient;
+
+namespace PatternDetectionEngine
+{
+    public static class DebugExtensions
+    {
+        public static string ToJsCommand(this TrafficPosition point)
+        {
+            return $"addPoint({point.Lat.ToString(CultureInfo.InvariantCulture)}, {point.Lon.ToString(CultureInfo.InvariantCulture)});";
+        }
+    }
+}
