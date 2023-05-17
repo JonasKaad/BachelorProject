909df997b60d62e554628777aa222d9f8b066ab3
Alexander Nørup <me@alexandernorup.com>
2023-05-17 10:31:14 +0200

    Adjust to 19 hours before waiting

--- FlightPatternDetection/Services/TrafficApiConstants.cs
+++ FlightPatternDetection/Services/TrafficApiConstants.cs
@@ -3,7 +3,7 @@
     public class TrafficApiConstants
     {
         public const int DaysDataIsKept = 6;
-        public const int HoursToWaitBeforeAnalyzingFlight = 13;
+        public const int HoursToWaitBeforeAnalyzingFlight = 19;
         public const int MaxFlightsToAnalyzeAtOneTime = 1500;
     }
 }
