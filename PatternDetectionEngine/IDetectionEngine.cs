09a98d10dcd328ec97093687cd43199b534a770d
VictorABoye <vandreasboye@gmail.com>
2023-05-22 09:50:53 +0200

    Added interface between engine and api

---
+++ PatternDetectionEngine/IDetectionEngine.cs
@@ -0,0 +1,12 @@
+﻿using FlightPatternDetection.DTO;
+using TrafficApiClient;
+
+namespace PatternDetectionEngine
+{
+    public interface IDetectionEngine
+    {
+        HoldingResult AnalyseFlight(List<TrafficPosition> flightData);
+        bool FlightIsFiltered(List<TrafficPosition> flightData);
+        List<TrafficPosition> RemoveUnnecessaryPoints(List<TrafficPosition> flightData);
+    }
+}
